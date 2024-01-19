32bc15bf2978ba6226ba4436854db11d7fe24af3
Truelierr <156783949+Truelierr@users.noreply.github.com>
2024-01-19 11:33:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-console.log('server is starting') 
-
 var express = require('express');
-
 var app = express();
-
-app.get('/ping', function (req, res) {
-    res.send('hello logo')
+app.get('/ping', (req, res) =>{
+    res.send('hello ')
 })
 
-var server = app.listen(3000, listening);  
-function listening() {
-
-    console.log("listening. . .");
-}
-
-// app.use(express.static('website'));
+var server = app.listen(3000);
