5e8abf794f4cbd22674c6df4157abfbe164b4f3b
hanshul <141112472+hanshul22@users.noreply.github.com>
2024-02-15 17:15:34 +0530

    updated branch 12

--- Clint-Side/src/screens/Main Page/MainPage.jsx
+++ Clint-Side/src/screens/Main Page/MainPage.jsx
@@ -1,24 +1,30 @@
+
 import React from "react";
 import "./MainPage.css";
-// import FlipCard from ".../Components/FlipCard";
+import axios from "axios";
+import { useState, useEffect } from "react";
+// import data from "../../Data.json";
+// import FlipCard from "../../Components/Flip Card/FlipCard";
 
 export default function MainPage() {
-  const data =  [
-    {
-      Count: 1,
-      Name: "Fullmetal Alchemist",
-      ReleaseDate: "April 5, 2009",
-      Type: "Shounen, Adventure, Fantasy",
-      ImageUrl:
-        "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS1dx2Fp1m7fM6rXw09v9Li9_HkyDEMQrMJr466HdQCRovf3IPsv6GQg_2amx9b",
-      Description:
-        "Based on the manga by Hiromu Arakawa, this series follows the journey of two brothers, Edward and Alphonse Elric, as they seek the Philosopher's Stone to restore their bodies.",
-    }
-  ]
+  
+  const [data, setdata] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:700/")
+      .then((response) => {
+        setdata(response.data);
+        // console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <>
-    <div className="list">
+      <div className="flipCardDiv">{/* <FlipCard /> */}</div>
+      <div className="list">
         {data.map((data, index) => (
           <div className="maincontainer" key={index}>
             <div className="thecard">
